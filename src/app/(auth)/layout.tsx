ced2d80fa85d6b1fa482a1e714379da2dc50547d
Arthur Araujo <araujo.arthur.dev@gmail.com>
2025-06-21 07:21:18 -0300

    feat: add logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,10 +2,12 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 import StackLogoAnimation from '@/animations/StackLogoAnimation';
 import { Button } from '@/components/shadcn/button';
 import { Silk } from '@/components/react-bits/Silk/Silk';
+import { TypographyH3 } from '@/components/shadcn/typography';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -18,8 +20,21 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
             speed={2.8}
         />
         
-        <div className="self-center justify-center mx-4 md:m-0 w-full md:w-[60vw] h-[70vh] dark:bg-zinc-900 rounded-xl shadow-2xl overflow-hidden flex rounded-xl border border-zinc-200 dark:border-zinc-800"> 
-            <div className="hidden lg:flex min-w-1/2 bg-[#141414] flex items-center justify-center">
+        <div className="relative self-center justify-center mx-4 md:m-0 w-full md:w-[60vw] h-[70vh] dark:bg-zinc-900 rounded-xl shadow-2xl overflow-hidden flex rounded-xl border border-zinc-200 dark:border-zinc-800"> 
+            <div className="absolute top-4 left-4 z-10">
+                <div className="flex items-center gap-2">
+                    <Image
+                        src="/vicky-logo.svg"
+                        alt="Vicky Logo"
+                        width={26}
+                        height={26}
+                        className="self-end"
+                    />
+                    <TypographyH3>Vicky</TypographyH3>
+                </div>
+            </div>
+            
+            <div className="hidden lg:flex min-w-1/2 bg-[#141414] items-center justify-center">
                 <StackLogoAnimation />
             </div>
 
