914a4690b688296e3978eb02fec09c8ffad6d8a6
Arthur Araujo <araujo.arthur.dev@gmail.com>
2025-06-22 20:58:31 -0300

    feat: add tanstack query lib

--- src/animations/StackLogoAnimation.tsx
+++ src/animations/StackLogoAnimation.tsx
@@ -5,8 +5,8 @@ import { useLayoutEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { CSSPlugin } from "gsap/CSSPlugin";
 
-gsap.registerPlugin(CSSPlugin);
-gsap.config({ force3D: true });
+gsap.registerPlugin(CSSPlugin)
+gsap.config({ force3D: true })
 
 const StackLogoAnimation = () => {
   const barsRef = useRef<HTMLDivElement[]>([]);
@@ -20,10 +20,10 @@ const StackLogoAnimation = () => {
         y: (i) => i * -1,
         zIndex: (i) => 10 - i,
         backgroundColor: "#ffffff",
-        transformOrigin: "center center",
-      });
+        transformOrigin: "center center"
+      })
 
-      const tl = gsap.timeline({ delay: 0.3 });
+      const tl = gsap.timeline({ delay: 0.3 })
 
       tl.to(barsRef.current, {
         y: (i) => i * 36,
@@ -31,36 +31,20 @@ const StackLogoAnimation = () => {
         opacity: 1,
         duration: 0.8,
         ease: "power2.inOut",
-        stagger: 0.1,
+        stagger: 0.1
       })
 
-      .to(
-        barsRef.current,
-        {
-          y: (i) => [
-            -50,
-            -15,
-            0
-          ][i],
-          x: (i) => [
-            -60,
-            -20,
-            30
-          ][i],
-          rotationZ: (i) => [
-            -100,
-            -70,
-            -35
-          ][i],
-          duration: 0.8,
-          ease: "back.out(1.5)",
-        },
-        "+=0.4"
-      )
-    }, containerRef);
+      .to(barsRef.current, {
+        y: (i) => [-50, -10, 0][i],
+        x: (i) => [-60, -20, 30][i],
+        rotationZ: (i) => [-100, -70, -35][i],
+        duration: 0.6,
+      }, "+=0.4")
+    }, containerRef)
 
-    return () => ctx.revert();
-  }, []);
+    return () => ctx.revert()
+  }, [])
+
 
   return (
     <div ref={containerRef} className="relative w-32 h-32">
@@ -68,14 +52,14 @@ const StackLogoAnimation = () => {
         <div
           key={i}
           ref={(el) => {
-            if (el) barsRef.current[i] = el;
+            if (el) barsRef.current[i] = el
           }}
           style={{
-            transform: `translate(-50%, -50%) scale(${1 - i * 0.05})`,
+            transform: `translate(-50%, -50%) scale(${1 - i * 0.05 })`,
             opacity: `${1 - i * 0.15}`,
             top: `calc(50% + ${i * -4}px)`,
             zIndex: `${10 - i}`,
-            transformOrigin: "center center",
+            transformOrigin: "center center"
           }}
           className="absolute left-1/2 w-48 h-6 rounded bg-white"
         />
