e7523e6b74a888e0dfea61f10afb987b1df13f05
Arthur Araujo <araujo.arthur.dev@gmail.com>
2025-07-02 12:17:26 -0300

    feat: experience card component

---
+++ src/components/experience-card.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { Card, CardContent, CardHeader, CardFooter } from "@/components/shadcn/card"
+import { Skeleton } from "@/components/shadcn/skeleton"
+
+interface ExperienceCardProps {
+  children: React.ReactNode
+  className?: string
+}
+
+interface ExperienceCardWrapperProps {
+    children: React.ReactNode
+    isLoading: boolean
+}
+
+function ExperienceCardWrapper({ children, isLoading }: ExperienceCardWrapperProps) {
+  return (
+    <Card className={`hover:shadow-md transition-shadow gap-0 ${isLoading ? 'p-0' : ''}`}>
+      {isLoading ? (
+        <Skeleton className="h-64 w-full" />
+      ) : (
+        children
+      )}
+    </Card>
+  )
+}
+
+function ExperienceCardHeader({ children, className }: ExperienceCardProps) {
+  return (
+    <CardHeader className={className}>
+      {children}
+    </CardHeader>
+  )
+}
+
+function ExperienceCardContent({ children, className }: ExperienceCardProps) {
+  return (
+    <CardContent className={className}>
+      {children}
+    </CardContent>
+  )
+}
+
+function ExperienceCardFooter({ children, className }: ExperienceCardProps) {
+  return (
+    <CardFooter className={className}>
+      {children}
+    </CardFooter>
+  )
+}
+
+export const ExperienceCard = {
+  Wrapper: ExperienceCardWrapper,
+  Header: ExperienceCardHeader,
+  Content: ExperienceCardContent,
+  Footer: ExperienceCardFooter,
+}
